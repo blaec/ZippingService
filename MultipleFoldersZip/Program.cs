d2827cb2646ea511f107df06eac2f4ec95b81171
blaec <odeskonst@yandex.ru>
2023-04-21 21:26:50 +0300

    extract multiple folders zip code

---
+++ MultipleFoldersZip/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace MultipleFoldersZip
+{
+    /// <summary>
+    /// Iterate though all 1st level folders in source-folder-location
+    /// Get year from folder's last write time, create folder with this year and move all similar folders into this folder
+    /// Compress all newly created folders
+    /// Only skip folders created in current year
+    /// </summary>
+    internal class Program
+    {
+        private const string SourceLocation = @"C:\terminalCheck\logs\";
+        private static readonly int CurrentYear = DateTime.Today.Year;
+
+        public static void Main(string[] args)
+        {
+            var stopwatch = ZipFolders();
+            Console.WriteLine($"{stopwatch.Elapsed} | Done!");
+            Console.ReadLine();
+        }
+
+        private static Stopwatch ZipFolders()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            HashSet<int> years = new HashSet<int>();
+            
+            string[] zipFolders = Directory.GetDirectories(SourceLocation);
+            foreach (var zipFolder in zipFolders)
+            {
+                var year = Directory.GetLastWriteTime(zipFolder).Year;
+                if (CurrentYear == year) continue;
+
+                years.Add(year);
+                var destination = $"{SourceLocation}{year}{Path.DirectorySeparatorChar}";
+                Directory.CreateDirectory(destination);
+                Directory.Move(zipFolder, $"{destination}{Path.GetFileName(zipFolder)}");
+            }
+
+            foreach (var zippedFolder in years.Select(year => $"{SourceLocation}{year}"))
+            {
+                Console.WriteLine($"{stopwatch.Elapsed} | Compressing folder: {zippedFolder}...");
+                ZipFile.CreateFromDirectory(zippedFolder, $"{zippedFolder}.zip");
+                Console.WriteLine($"{stopwatch.Elapsed} | New zip file: {zippedFolder}.zip created.");
+            }
+
+            return stopwatch;
+        }
+    }
+}
